cdcfc8b7b4aa9ced5da49b21a862d3d605d8b160
Gabriel Teixeira de Souza <gabriel.teixeira@academico.ifrn.edu.br>
2022-11-21 10:23:25 -0300

    Projeto inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { VisualizarMensagemComponent } from './componentes/visualizar-mensagem/visualizar-mensagem.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VisualizarMensagemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/componentes/visualizar-mensagem/visualizar-mensagem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visualizar-mensagem',
+  templateUrl: './visualizar-mensagem.component.html',
+  styleUrls: ['./visualizar-mensagem.component.css']
+})
+export class VisualizarMensagemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
