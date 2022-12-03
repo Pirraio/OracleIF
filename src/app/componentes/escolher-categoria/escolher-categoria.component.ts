e0d962e37b5b6f82f1fdaef8d6b79999f0ddeed7
Pirraio <g.teixeira7x@gmail.com>
2022-12-02 21:47:50 -0300

    CSS e criação de componentes

---
+++ src/app/componentes/escolher-categoria/escolher-categoria.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-escolher-categoria',
+  templateUrl: './escolher-categoria.component.html',
+  styleUrls: ['./escolher-categoria.component.css']
+})
+export class EscolherCategoriaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
