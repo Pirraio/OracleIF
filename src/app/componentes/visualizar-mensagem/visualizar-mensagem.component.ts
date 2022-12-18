3ac16e56ec2482ef6b20154a3eea691e4565f362
Pirraio <g.teixeira7x@gmail.com>
2022-12-18 11:20:46 -0300

    Pré-Pull no Github

--- src/app/componentes/visualizar-mensagem/visualizar-mensagem.component.ts
+++ src/app/componentes/visualizar-mensagem/visualizar-mensagem.component.ts
@@ -7,16 +7,13 @@ import { APIMensagensService } from '../../model/api-mensagens.service';
   styleUrls: ['./visualizar-mensagem.component.css']
 })
 export class VisualizarMensagemComponent implements OnInit {
-  piada: string
+
   constructor(private pi: APIMensagensService) { 
-    this.piada = ''
+
   }
     ngOnInit(): void {
       
   }
-  contarPiada() {
-    this.piada = 'piada'
-    this.pi.contarPiada(this.piada)
-  }
+
 }
 
