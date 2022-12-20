1e288e8b03dd92f51cf9bd29ec0b4d8c6652d822
Arthur Emanuel Souza Cassiano da Costa <arthurcosta0ac@gmail.com>
2022-12-20 10:46:22 -0300

    Configurando página sobre e de edição de mensagens e categorias

--- src/app/componentes/editor-mensagem/editor-mensagem.component.ts
+++ src/app/componentes/editor-mensagem/editor-mensagem.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { APIMensagensService } from 'src/app/model/api-mensagens.service';
+import { Categoria } from 'src/app/model/categoria';
 
 @Component({
   selector: 'app-editor-mensagem',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editor-mensagem.component.css']
 })
 export class EditorMensagemComponent implements OnInit {
+  categorias: Categoria[];
+  //categoriaForm: FormGroup;
 
-  constructor() { }
+  constructor(private api: APIMensagensService/*,
+              private fb: FormBuilder*/)
+  {
+    /*this.categoriaForm = this.fb.group({
+      nomeCat: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(30)]]
+    });*/
+    this.categorias = [];
+  }
 
   ngOnInit(): void {
+    this.api.obterCategorias().subscribe(res => {
+      this.categorias = res;
+    });
   }
 
 }
