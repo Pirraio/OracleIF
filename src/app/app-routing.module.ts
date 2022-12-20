44fc0a769e28712360d5aff13f8fdc9bbd063e74
Arthur Emanuel Souza Cassiano da Costa <arthurcosta0ac@gmail.com>
2022-12-20 09:18:10 -0300

    Criando componente da página de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { SobreComponent } from './componentes/sobre/sobre.component';
 import { PaginaNaoEncontradaComponent } from './componentes/pagina-nao-encontrada/pagina-nao-encontrada.component';
 import { ChatboxComponent } from './componentes/chatbox/chatbox.component';
+import { LoginComponent } from './componentes/login/login.component';
 
 
 const routes: Routes = [
   { path: 'home', component: ChatboxComponent},
   { path: 'sobre', component: SobreComponent},
+  { path: 'login', component: LoginComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: '**', component: PaginaNaoEncontradaComponent}
 ];
