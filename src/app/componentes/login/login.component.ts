515ffa5550ad188ec71d1b8aea01e82296aa500a
Arthur Emanuel Souza Cassiano da Costa <arthur.emanuel@escolar.ifrn.edu.br>
2022-12-21 10:24:31 -0300

    Configurando componente de login

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { APIMensagensService } from 'src/app/model/api-mensagens.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  dadosUsuarioForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+              private api: APIMensagensService,
+              private rt: Router)
+  {
+    this.dadosUsuarioForm = this.fb.group({
+      nomeUsuario: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
+      senhaUsuario: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]]
+    });
+  }
 
   ngOnInit(): void {
+    const usuario: string | null = sessionStorage.getItem("nomeUsuario");
+    if (usuario == "" || usuario == null) {
+      
+    }
   }
 
 }
